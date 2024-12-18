573c9d970cb5364008acad5982844d6fac2d8a05
andre2905-code <andresreis.2018@gmail.com>
2024-12-18 18:36:11 -0300

    First commit

---
+++ widgets/header.php
@@ -0,0 +1,41 @@
+<head>
+  <link rel="stylesheet" href="../assets/styles/header.css">
+  <link rel="stylesheet" href="../assets/styles/global.css">
+</head>
+
+<header>
+  <form action="">
+    <div class="container">
+      <div class="left">
+        <div class="logo">
+          <img src="../assets/images/logo-techabc.png" alt="logo-techabc">
+        </div>
+      </div>
+      <div class="center">
+        <div class="search-bar">
+          <input type="text" placeholder="Pesquise aqui...">
+          <button id="search-button" class="search" type="submit">Pesquisar</button>
+        </div>
+        <nav id="menu" class="navbar">
+          <ul>
+            <li>Home</li>
+            <li>Sobre</li>
+            <li>Contato</li>
+          </ul>
+        </nav>
+      </div>
+      <div class="right">
+        <div class="utility">
+          <div class="login-section">
+            <button>Login</button>
+            <button>Cadastrar</button>
+          </div>
+          <div class="user-actions">
+            <button class="cart">Carrinho</button>
+            <button class="favorites">Favoritos</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </form>
+</header>
